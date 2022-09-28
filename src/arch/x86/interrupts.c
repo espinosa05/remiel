5a7db624d58635119260251f97506537bb02604e
Francisco Fischer <80589591+ffischer05@users.noreply.github.com>
2022-09-28 10:48:55 +0200

    Update interrupts.c

--- src/arch/x86/interrupts.c
+++ src/arch/x86/interrupts.c
@@ -8,7 +8,7 @@ void set_idt_entry(struct idt_entry *idt_descriptor, void (*handler_address_32) 
   idt_descriptor->code_segment_selector = KERNEL_CODE_SEGMENT_SELECTOR;
   idt_descriptor->reserved = 0;
   idt_descriptor->attributes = attributes;
-  idt_descriptor->offset_high = ((0xffff) & handler_address_32);
+  idt_descriptor->offset_high = (~(0xffff << 16) & handler_address_32);
 }
 
 void set_idt_descriptor(struct idtr *idt_register, struct idt_entry *table_address, uint16_t sizeof_idt) {
